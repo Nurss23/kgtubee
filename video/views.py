e54c65cedf5850a0dde30fb56a5f6fff3268362c
Nurs <nurss230101@gmail.com>
2023-11-10 12:21:26 +0600

    add videos

--- video/views.py
+++ video/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render
 from .models import Video
 
 # Create your views here.
+# def video(request):
+#     query_result = Video.objects.all()
+
+#     context = {"objects_list": query_result}
+
+#     return render(
+#         request,
+#         'videos.html',
+#         context
+#     )
+
 def videos(request):
-    query_result = Video.objects.all()
+    videos_list = Video.objects.all()
+    context = {"videos_list": videos_list}
+    return render(request, 'videos.html', context)
 
-    context = {"objects_list": query_result}
-
-    return render(
-        request,
-        'videos.html',
-        context
-    )
 
 def video(request, id):
     video_object = Video.objects.get(id=id)
